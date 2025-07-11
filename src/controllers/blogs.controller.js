62fffad8af4475fea690e2abb834a2fb5f1e0c27
王昱 <wangyu2021@foxmail.com>
2025-07-11 17:11:42 +0300

    13.14

--- src/controllers/blogs.controller.js
+++ src/controllers/blogs.controller.js
@@ -7,8 +7,13 @@ export const getBlogs = async (req, res) => {
   let where = {};
   if (req.query.search) {
     where = {
-      title: {
-        [Op.iLike]: `%${req.query.search}%`,
+      [Op.or]: {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+        author: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
       },
     };
   }
